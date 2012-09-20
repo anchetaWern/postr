3507ecaa892fe41a6e0e4a47ad95bb67d6ce802e
wern ancheta <ancheta.wern@gmail.com>
2012-09-20 22:35:35 +0800

    add reference to jquery ui

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
 		<link rel="stylesheet" href="/postr/libs/noty/css/jquery.noty.css"/>
 		<link rel="stylesheet" href="/postr/libs/noty/css/noty_theme_default.css"/>
 		<link rel="stylesheet" href="/postr/css/main.css"/>
+		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/base/jquery-ui.css"/>
 		
 		<link rel="postr_icon" href="/postr/img/postr.ico">
 
