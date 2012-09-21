0dc8ef052bce7023cc05185602ebe7338154c08b
wern ancheta <ancheta.wern@gmail.com>
2012-09-21 17:21:12 +0800

    remove code for accessing user setting on database

--- actions/actions.php
+++ actions/actions.php
@@ -19,16 +19,7 @@ switch($action){
 			$query->bind_param("sss", $email, $hash, $salt);
 			$query->execute();
 			$uid = $query->insert_id;
-			
-			$get_networks = $db->query("SELECT sid FROM tbl_settings");
-			if($get_networks->num_rows > 0){
-				
-				while($row = $get_networks->fetch_object()){
-					$sid = $row->sid;
-					$db->query("INSERT INTO tbl_usersettings SET uid = '$uid', sid = '$sid', status = 0");
-				}
-			
-			}
+		
 			echo $uid;
 		}
 	
@@ -56,28 +47,6 @@ switch($action){
 			}
 		}
 		
-	break;
-	
-	case 'set':
-		$uid = $_SESSION['uid'];
-		$sid = $_POST['sid'];
-		$status = $_POST['status'];
-		
-		$db->query("UPDATE tbl_usersettings SET status = '$status' WHERE uid = '$uid' AND sid = '$sid'");
-	break;
-	
-	case 'load_set':
-		$uid = $_SESSION['uid'];
-		$settings = [];
-		$get_settings = $db->query("SELECT sid, status FROM tbl_usersettings WHERE uid = '$uid'");
-		if($get_settings->num_rows > 0){
-			while($row = $get_settings->fetch_object()){
-				$sid = $row->sid;
-				$status = $row->status;
-				$settings[] = ['sid'=>$sid, 'status'=>$status];
-			}
-		}
-		echo json_encode($settings);
 	break;
 	
 	case 'get_uid':
