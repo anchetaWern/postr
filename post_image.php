8a4168b8b79868a601d43d78afd68173c6a56d97
wern ancheta <ancheta.wern@gmail.com>
2012-09-27 11:27:14 +0800

    fixed bug on posting of status with images

--- post_image.php
+++ post_image.php
@@ -32,46 +32,53 @@ if(!empty($_POST['filename'])){
 $facebook->setFileUploadSupport(true);
 $facebook_setting = $_POST['fb_setting'];
 
-if($facebook_setting == 1){
-	$response = $facebook->api(
-		'/me/photos/',
-		'post',
-		array(
-			'message' => $message,
-			'source' => '@'.$file
-		)
-	);
-}
-
-$groups = $_POST['fb_groups'];
-foreach($groups as $group_id=>$group){
-	$group_status = $group['group_status'];
-	
-	if($group_status == 1){
+if(!empty($file)){
+	if($facebook_setting == 1){
 		$response = $facebook->api(
-			"/$group_id/photos/",
-			"post",
+			'/me/photos/',
+			'post',
 			array(
-				"message" => $message,
-				"source" => '@'.$file
+				'message' => $message,
+				'source' => '@'.$file
 			)
 		);
 	}
 }
 
+$groups = $_POST['fb_groups'];
+
+if(!empty($file)){
+	foreach($groups as $group_id=>$group){
+		$group_status = $group['group_status'];
+		
+		if($group_status == 1){
+			$response = $facebook->api(
+				"/$group_id/photos/",
+				"post",
+				array(
+					"message" => $message,
+					"source" => '@'.$file
+				)
+			);
+		}
+	}
+}
+
 $pages = $_POST['fb_pages'];
-foreach($pages as $page_id=>$page){
-	if($page['page_status'] == 1){
-		$page_data = $facebook->api("/$page_id", array("fields" => "access_token"));
-		$page_access_token = $page_data['access_token'];
-		
-		$contents = array(
-			"access_token"=>$page_access_token, 
-			"message"=>$message, 
-			"source"=>'@'.$file
-		);
-		
-		$post_response = $facebook->api("/$page_id/photos", "post", $contents);
+if(!empty($file)){
+	foreach($pages as $page_id=>$page){
+		if($page['page_status'] == 1){
+			$page_data = $facebook->api("/$page_id", array("fields" => "access_token"));
+			$page_access_token = $page_data['access_token'];
+			
+			$contents = array(
+				"access_token"=>$page_access_token, 
+				"message"=>$message, 
+				"source"=>'@'.$file
+			);
+			
+			$post_response = $facebook->api("/$page_id/photos", "post", $contents);
+		}
 	}
 }
 
